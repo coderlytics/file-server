2f1acdd2749bfe004c5476292c6dd599677d9c6f
msieber88 <msieber88@gmail.com>
2021-11-29 08:26:16 +0100

    add logging information

--- internal/server/server.go
+++ internal/server/server.go
@@ -38,18 +38,20 @@ func run() {
 	router := chi.NewRouter()
 	router.Use(middleware.Logger)
 	buildRoutes(router)
+	log.Info(fmt.Sprintf("Starting server on port %s", Config.FileServer.Port))
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", Config.FileServer.Port), router))
 }
 
 // buildRoots sets up all routes from the configuration file
 func buildRoutes(router chi.Router) {
 	for _, file := range Config.Files {
+		log.Debugf("Adding endpoint %s for file %s", file.Endpoint, file.FilePath)
 
 		router.Get(file.Endpoint, func(file File) func(rw http.ResponseWriter, r *http.Request) {
 			return func(rw http.ResponseWriter, r *http.Request) {
 				// if token is set, authorization check will be performed
 				if len(file.Token) > 0 {
-					println("auth required")
+					log.Debugf("Adding authentication to endpoint %s", file.Endpoint)
 					auth := r.Header.Get("Authorization")
 
 					if file.Token != auth {
